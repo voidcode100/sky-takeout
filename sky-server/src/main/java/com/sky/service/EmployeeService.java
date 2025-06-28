5933ea5e8bfe21374104a31379f4ddd546e47c96
voidcode100 <spunckcore@gamil.com>
2025-06-28 22:03:08 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.sky.service;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
@@ -18,4 +21,12 @@ public interface EmployeeService {
      * @param employeeDTO
      */
     void save(EmployeeDTO employeeDTO);
+
+
+    /**
+     * 分页查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult PageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
